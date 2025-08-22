e6bb0cbd5d5740f76f166c1d00a408c404cd7314
hancco05 <s.carmonawright@gmail.com>
2025-08-21 23:54:40 -0400

    commi3

---
+++ src/main/java/com/example/animal_conciencia/AnimalConcienciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal_conciencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalConcienciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalConcienciaApplication.class, args);
+	}
+
+}
