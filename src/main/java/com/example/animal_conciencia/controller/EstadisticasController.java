e0b3a1f0ec6d8ecd6c5d843a01105ef9bb7985fc
hancco05 <s.carmonawright@gmail.com>
2025-08-22 23:59:57 -0400

    commi9

--- src/main/java/com/example/animal_conciencia/controller/EstadisticasController.java
+++ src/main/java/com/example/animal_conciencia/controller/EstadisticasController.java
@@ -1,5 +1,18 @@
-package com.example.animal_conciencia.controller;
+package com.ejemplo.animalconciencia.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+@RestController
 public class EstadisticasController {
 
+    @GetMapping("/api/estadisticas")
+    public Map<String, Object> getEstadisticas() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("animales_rescatados", 2500);
+        data.put("denuncias_2024", 830);
+        data.put("adopciones", 1200);
+        return data;
+    }
 }
