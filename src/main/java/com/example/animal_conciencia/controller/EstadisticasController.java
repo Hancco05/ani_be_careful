fba51df7bc34346993b28c73369598c8eb63adc5
hancco05 <s.carmonawright@gmail.com>
2025-08-25 23:37:27 -0400

    commi0.1

--- src/main/java/com/example/animal_conciencia/controller/EstadisticasController.java
+++ src/main/java/com/example/animal_conciencia/controller/EstadisticasController.java
@@ -1,4 +1,4 @@
-package com.ejemplo.animalconciencia.controller;
+package com.example.animal_conciencia.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
