9aec396e5714994cdc0574b61473a0f97025f8f1
hancco05 <s.carmonawright@gmail.com>
2025-08-25 23:57:47 -0400

    commi0.4

--- src/main/java/com/example/animal_conciencia/controller/HomeController.java
+++ src/main/java/com/example/animal_conciencia/controller/HomeController.java
@@ -1,13 +1,33 @@
 package com.example.animal_conciencia.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "index"; // Renderiza index.html en templates
+    public String index(Model model) {
+        model.addAttribute("title", "Cuidemos a los Animales");
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Sobre Nosotros");
+        return "about";
+    }
+
+    @GetMapping("/actions")
+    public String actions(Model model) {
+        model.addAttribute("title", "Acciones y Consejos");
+        return "actions";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("title", "Contacto");
+        return "contact";
     }
 }
