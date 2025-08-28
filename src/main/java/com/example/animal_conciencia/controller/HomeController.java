b2973f30842a5d9c7389b56a0363e5ebf40d9144
hancco05 <s.carmonawright@gmail.com>
2025-08-27 23:45:54 -0400

    commi0.7

--- src/main/java/com/example/animal_conciencia/controller/HomeController.java
+++ src/main/java/com/example/animal_conciencia/controller/HomeController.java
@@ -9,25 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("title", "Cuidemos a los Animales");
+        model.addAttribute("title", "Conciencia Animal");
         return "index";
-    }
-
-    @GetMapping("/about")
-    public String about(Model model) {
-        model.addAttribute("title", "Sobre Nosotros");
-        return "about";
-    }
-
-    @GetMapping("/actions")
-    public String actions(Model model) {
-        model.addAttribute("title", "Acciones y Consejos");
-        return "actions";
-    }
-
-    @GetMapping("/contact")
-    public String contact(Model model) {
-        model.addAttribute("title", "Contacto");
-        return "contact";
     }
 }
